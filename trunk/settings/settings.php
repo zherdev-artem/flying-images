49d7cb37ef72141ecc3f369e9b110eedcc0a41eb
Gijo Varghese <gijovarghese141@gmail.com>
2019-12-19 13:34:18 +0530

    v2.0

--- trunk/settings/settings.php
+++ trunk/settings/settings.php
@@ -1,4 +1,4 @@
-<h2>Flying Images Settings</h2>
+<h2>Flying Images by WP Speed Matters Settings</h2>
 
 <?php
     include('lazyload.php');
@@ -6,7 +6,9 @@
     include('compression.php');
     include('responsiveness.php');
     include('webp.php');
+    include('faq.php');
     include('support.php');
+    include('optimize-more.php');
 
     $active_tab = isset($_GET['tab']) ? $_GET['tab'] : "lazyload";
 
@@ -18,10 +20,12 @@
 <h2 class="nav-tab-wrapper">
     <a href="?page=flying-images&tab=lazyload" class="nav-tab <?php echo $active_tab == 'lazyload' ? 'nav-tab-active' : ''; ?>">Lazy load</a>
     <a href="?page=flying-images&tab=cdn" class="nav-tab <?php echo $active_tab == 'cdn' ? 'nav-tab-active' : ''; ?>">CDN</a>
-    <a href="?page=flying-images&tab=compression" class="nav-tab <?php echo $active_tab == 'compression' ? 'nav-tab-active' : ''; ?>">Compression</a>
     <a href="?page=flying-images&tab=responsiveness" class="nav-tab <?php echo $active_tab == 'responsiveness' ? 'nav-tab-active' : ''; ?>">Responsiveness</a>
+    <a href="?page=flying-images&tab=compression" class="nav-tab <?php echo $active_tab == 'compression' ? 'nav-tab-active' : ''; ?>">Compression</a>
     <a href="?page=flying-images&tab=webp" class="nav-tab <?php echo $active_tab == 'webp' ? 'nav-tab-active' : ''; ?>">WebP</a>
+    <a href="?page=flying-images&tab=faq" class="nav-tab <?php echo $active_tab == 'faq' ? 'nav-tab-active' : ''; ?>">FAQ</a>
     <a href="?page=flying-images&tab=support" class="nav-tab <?php echo $active_tab == 'support' ? 'nav-tab-active' : ''; ?>">Support Us</a>
+    <a href="?page=flying-images&tab=optimize-more" class="nav-tab <?php echo $active_tab == 'optimize-more' ? 'nav-tab-active' : ''; ?>">Optimize More!</a>
 </h2>
 
 <?php
@@ -41,8 +45,14 @@
         case 'webp':
             flying_pages_settings_webp();
             break;
+        case 'faq':
+            flying_pages_settings_faq();
+            break;
         case 'support':
             flying_pages_settings_support();
+            break;
+        case 'optimize-more':
+            flying_pages_settings_optimize_more();
             break;
         default:
             flying_pages_settings_lazy_load();
